b469d870eac465f1470c2683091ae4c1dfcd0e62
Jeff Newmiller <jdnewmil@gmail.com>
2021-12-02 01:02:22 -0800

    2021 Day 2

--- R/adventofcode_2021.R
+++ R/adventofcode_2021.R
@@ -5,3 +5,49 @@
 count_increases <- function( x ) {
   sum( 1 == ( 0 < diff( x ) ) )
 }
+
+# Day 2 ----
+
+parse_subfile <- function( text ) {
+  read.table( text = text ) |> setNames( c( "dir", "dist" ) )
+}
+
+augment_subfile_day2 <- function( sf_df ) {
+  sf_df <- merge( sf_df, data.frame( dir = c( "forward", "down", "up" )
+                                   , dir_x = c( 1, 0, 0 )
+                                   , dir_y = c( 0, 1, -1 )
+                                   ))
+  sf_df$dir_x <- with( sf_df, dir_x * dist )
+  sf_df$dir_y <- with( sf_df, dir_y * dist )
+  sf_df
+}
+
+calc_pos_2a <- function( text ) {
+  (  text
+  |> parse_subfile()
+  |> augment_subfile_day2()
+  |> subset( select = dir_x:dir_y )
+  |> lapply( sum )
+  )
+}
+
+calc_pos_2b <- function( text ) {
+  coords <- (  text
+            |> parse_subfile()
+            )
+  x <- y <- aim <- 0
+  for ( i in seq.int( nrow( coords ) ) ) {
+    tmp <- coords[[ "dist" ]][ i ]
+    if ( "forward" == ( tmpdir <- coords[[ "dir" ]][ i ] ) ) {
+      x <- tmp + x
+      y <- y + tmp * aim
+    } else if ( "down" == tmpdir ) {
+      aim <- aim + tmp
+    } else if ( "up" == tmpdir ) {
+      aim <- aim - tmp
+    } else {
+      stop( "invalid direction" )
+    }
+  }
+  data.frame( x = x, y = y )
+}
--- tests/test_adventofcode_2021.R
+++ tests/test_adventofcode_2021.R
@@ -21,3 +21,30 @@ test_that( "count_increases", {
   expect_equal( 7, ans )
 })
 
+# Day 2 ----
+
+test_that( "calc_pos_2a", {
+  ex2a <-
+"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+  ans <- calc_pos_2a( ex2a )
+  expect_equal( ans$dir_x, 15 )
+  expect_equal( ans$dir_y, 10 )
+})
+
+test_that( "calc_pos_2b", {
+  ex2a <-
+    "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+  ans <- calc_pos_2b( ex2a )
+  expect_equal( ans$x, 15 )
+  expect_equal( ans$y, 60 )
+})
