520fcaebe186e111a96a85dcfcefd7b81ba09892
Jeff Newmiller <jdnewmil@gmail.com>
2021-12-03 00:37:31 -0800

    2021 Day 3

--- R/adventofcode_2021.R
+++ R/adventofcode_2021.R
@@ -51,3 +51,76 @@ calc_pos_2b <- function( text ) {
   }
   data.frame( x = x, y = y )
 }
+
+# Day 3 ----
+
+cvt_bin_vec_2_int <- function( v ) {
+  Reduce( \(n, dig) { n <- 2 * n + ( "0" != dig )}, v, init = 0L )
+}
+
+cvt_lgl_vec_2_int <- function( v ) {
+  Reduce( \(n, dig) { n <- 2 * n + dig }, v, init = 0L )
+}
+
+parse_bin <- function( binv ) {
+  (  binv
+  |> strsplit( "", fixed = TRUE )
+  |> sapply( cvt_bin_vec_2_int )
+  )
+}
+
+
+parse_lgl_columns_m <- function( binv ) {
+  (  binv
+  |> strsplit( "", fixed = TRUE )
+  |> (\(x) do.call( rbind, x ))()
+  )
+}
+
+parse_lgl_columns <- function( binv ) {
+  (  binv
+  |> parse_lgl_columns_m()
+  |> (\(x) lapply( seq.int( ncol( x ) )
+                 , \(y) (nrow(x)/2) <= unname(table(x[ , y ])["1"])
+                 ))()
+  |> unlist()
+  )
+}
+
+calc_gamma_rate <- function( lv ) {
+  cvt_lgl_vec_2_int( lv )
+}
+
+calc_epsilon_rate <- function( lv ) {
+  cvt_lgl_vec_2_int( !lv )
+}
+
+test_oxy_gen_criteria <- function( v ) {
+  most_common <- attr( which.max( rev( table( v ) ) )[ 1 ], "names" ) == "TRUE"
+  v == most_common
+}
+
+test_CO2_scrubber_criteria <- function( v ) {
+  !test_oxy_gen_criteria( v )
+}
+
+calc_CO2_scrubber_rating <- function( m ) {
+  calc_day3_rating( m, f = test_CO2_scrubber_criteria )
+}
+
+calc_oxy_gen_rating <- function( m ) {
+  calc_day3_rating( m, f = test_oxy_gen_criteria )
+}
+
+calc_day3_rating <- function( m, f ) {
+  m <- matrix( "1" == m, ncol = ncol( m ) )
+  n <- ncol( m )
+  i <- 0
+  sel <- rep( TRUE, nrow( m ) )
+  while( i < n && 1L < sum( sel ) ) {
+    i <- i + 1L
+    sel[ sel ] <- f( m[ sel, i ] )
+  }
+  stopifnot( 1L == sum( sel ) )
+  cvt_lgl_vec_2_int( m[ sel, ] )
+}
--- tests/test_adventofcode_2021.R
+++ tests/test_adventofcode_2021.R
@@ -5,7 +5,7 @@ suppressPackageStartupMessages({
   library(dplyr)
   library(tidyr)
   library(purrr)
-  #library(gmp)
+  library(gmp)
 })
 
 rdir <- "../R"
@@ -47,4 +47,121 @@ forward 2"
   ans <- calc_pos_2b( ex2a )
   expect_equal( ans$x, 15 )
   expect_equal( ans$y, 60 )
+})
+
+# Day 3 ----
+
+test_that( "read_binary_vec", {
+  lns <-
+"00100
+11110
+10110
+10111
+10101"
+  ans <- (  scan( text = lns, what = "character", quiet = TRUE )
+         |> parse_bin()
+         )
+  expect_equal( ans, c( 4, 30, 22, 23, 21 ) )
+})
+
+test_that( "calc_gamma_rate", {
+  lns <-
+"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+  ans <- (  scan( text = lns, what = "character", quiet = TRUE )
+         |> parse_lgl_columns()
+         |> calc_gamma_rate()
+         )
+  expect_equal( ans, 22 )
+})
+
+test_that( "calc_epsilon_rate", {
+  lns <-
+"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+  ans <- (  scan( text = lns, what = "character", quiet = TRUE )
+         |> parse_lgl_columns()
+         |> calc_epsilon_rate()
+  )
+  expect_equal( ans, 9 )
+})
+
+test_that( "test_oxy_gen_criteria", {
+  ans1 <- test_oxy_gen_criteria( c( TRUE, FALSE, FALSE, FALSE ) )
+  expect_equal( ans1, c( FALSE, TRUE, TRUE, TRUE ) )
+  ans2 <- test_oxy_gen_criteria( c( TRUE, TRUE, FALSE, FALSE ) )
+  expect_equal( ans2, c( TRUE, TRUE, FALSE, FALSE ) )
+  ans3 <- test_oxy_gen_criteria( c( TRUE, TRUE, TRUE, FALSE ) )
+  expect_equal( ans3, c( TRUE, TRUE, TRUE, FALSE ) )
+})
+
+test_that( "test_CO2_scrubber_criteria", {
+  ans1 <- test_CO2_scrubber_criteria( c( TRUE, FALSE, FALSE, FALSE ) )
+  expect_equal( ans1, c( TRUE, FALSE, FALSE, FALSE ) )
+  ans2 <- test_CO2_scrubber_criteria( c( TRUE, TRUE, FALSE, FALSE ) )
+  expect_equal( ans2, c( FALSE, FALSE, TRUE, TRUE ) )
+  ans3 <- test_CO2_scrubber_criteria( c( TRUE, TRUE, TRUE, FALSE ) )
+  expect_equal( ans3, c( FALSE, FALSE, FALSE, TRUE ) )
+})
+
+test_that( "calc_CO2_scrubber_rating", {
+  lns <-
+"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+  ans <- (  scan( text = lns, what = "character", quiet = TRUE )
+         |> parse_lgl_columns_m()
+         |> calc_CO2_scrubber_rating()
+         )
+  expect_equal( ans, 10L )
+})
+
+test_that( "calc_oxy_gen_rating", {
+  lns <-
+    "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+  ans <- (  scan( text = lns, what = "character", quiet = TRUE )
+            |> parse_lgl_columns_m()
+            |> calc_oxy_gen_rating()
+  )
+  expect_equal( ans, 23L )
 })
